874429c11f9e5f98fa71ce69f15987456df91a11
YaCpotato <yasshisshy@gmail.com>
2020-08-23 20:58:59 +0900

    template誘導がうまくいかない

--- mysite/todo/models.py
+++ mysite/todo/models.py
@@ -1,3 +1,15 @@
 from django.db import models
+from django.utils import timezone
 
 # Create your models here.
+
+class Todo(models.Model):
+    name = models.CharField(max_length=64)
+    start = models.DateField(default=timezone.now)
+    end = models.DateField(null=True,blank=True)
+    created_date = models.DateTimeField(
+            default=timezone.now)
+    updated_date = models.DateTimeField(
+            null=True,blank=True)
+    finished_date = models.DateTimeField(
+            null=True,blank=True)
--- mysite/todo/views.py
+++ mysite/todo/views.py
@@ -1,3 +1,7 @@
 from django.shortcuts import render
+from django.utils import timezone
 
 # Create your views here.
+
+def todo_list(request):
+    return render(request, 'todo/todo_list.html')
