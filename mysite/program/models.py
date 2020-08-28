50ec9e54fed5ab3beef77273ecd3ed89bbd7086a
YaCpotato <yasshisshy@gmail.com>
2020-08-28 19:57:42 +0900

    テンプレート、view作成

--- mysite/program/models.py
+++ mysite/program/models.py
@@ -6,12 +6,15 @@ from django.utils import timezone
 # Master models
 
 class Program(models.Model):
+    id = models.AutoField(primary_key=True)
     name = models.CharField(max_length=64)
     discription = models.CharField(max_length=256)
     created_date = models.DateTimeField(default=timezone.now,editable=False)
     updated_date = models.DateTimeField(null=True,blank=True)
 
 class Course(models.Model):
+    id = models.AutoField(primary_key=True)
+    program = models.ForeignKey('program.Program', on_delete=models.CASCADE, related_name='courses')
     name = models.CharField(max_length=64)
     discription = models.CharField(max_length=256)
     end = models.DateField(null=True,blank=True)
@@ -25,6 +28,8 @@ class Course(models.Model):
     #FIXME 何Lessonあるか計上する関数
 
 class Lesson(models.Model):
+    id = models.AutoField(primary_key=True)
+    course = models.ForeignKey('program.Course', on_delete=models.CASCADE, related_name='lessons')
     name = models.CharField(max_length=64)
     code = models.IntegerField(default=0)
     discription = models.CharField(max_length=256)
--- mysite/program/views.py
+++ mysite/program/views.py
@@ -1,7 +1,13 @@
 from django.shortcuts import render
 from django.utils import timezone
+from .models import Program
 
 # Create your views here.
 
 def top(request):
     return render(request, 'program/search_header.html')
+
+def program_list(request):
+    programs = Program.objects.all().order_by('id')
+    return render(request, 'program/program_list.html', {'programs': programs})
+
