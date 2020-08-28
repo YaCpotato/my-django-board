420a773a1af2aeab03916d2b07cbfdcebd09069a
YaCpotato <yasshisshy@gmail.com>
2020-08-28 20:12:53 +0900

    admin.py作成

--- mysite/program/admin.py
+++ mysite/program/admin.py
@@ -1,3 +1,10 @@
 from django.contrib import admin
 
 # Register your models here.
+
+
+from .models import Program,Course,Lesson
+
+admin.site.register(Program)
+admin.site.register(Course)
+admin.site.register(Lesson)
