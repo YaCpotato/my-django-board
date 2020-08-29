c2a1489637a966962d533b4a6b113fb0b2218e05
YaCpotato <yasshisshy@gmail.com>
2020-08-29 17:52:55 +0900

    個別画面作成

--- mysite/program/urls.py
+++ mysite/program/urls.py
@@ -3,5 +3,6 @@ from . import views
 
 urlpatterns = [
     path('', views.top, name='top'),
-    path('program/',views.program_list, name='program_list')
+    path('program/',views.program_list, name='program_list'),
+    path('program/<int:pk>/', views.program_detail, name='program_detail'),
 ]
--- mysite/program/views.py
+++ mysite/program/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import render,get_object_or_404
 from django.utils import timezone
 from .models import Program
 
@@ -11,3 +11,7 @@ def program_list(request):
     programs = Program.objects.all().order_by('id')
     return render(request, 'program/program_list.html', {'programs': programs})
 
+
+def program_detail(request, pk):
+    program = get_object_or_404(Program, pk=pk)
+    return render(request, 'program/program_detail.html', {'program': program})
